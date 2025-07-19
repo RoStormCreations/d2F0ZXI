a14120b7b2755a72173df9b6aac5f7a82650c701
re_sistance <rostormstudios@gmail.com>
2025-07-19 14:19:42 -0400

    Update source.lua

--- holder/source.lua
+++ holder/source.lua
@@ -1,6 +1,6 @@
 local root = script.parent
-local RSC = require(root.resources)
-local UILibrary = require(RSC.venyx)
+local resources = require(root.resources)
+local UILibrary = require(resources.venyx)
 
 local Library = UILibrary.new("Water", 5013109572)
 local page = Library:addPage("Main", 5012544693)
